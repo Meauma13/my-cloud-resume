7ae356e363351eeddc3b2f9ce94e4ffa66168949
Meauma13 <86557478+Meauma13@users.noreply.github.com>
2023-12-16 22:48:45 +0100

    updates certs and work experience

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,3 +29,53 @@ scrollLinks.forEach((link) => {
         });
     });
 });
+
+let message = "Number of visits to this page - ";
+let countVisitsElement = document.querySelector("#countPageViews");
+let countVisits;
+
+// first check if local storage exists, then next return the value for the pageViews key if exists
+function numVisits() {
+    if (typeof Storage !== "undefined") {
+        countVisits = window.localStorage.getItem("pageViews");
+        return countVisits;
+    } else {
+        document.getElementById("pageView").remove();
+        return false;
+    }
+}
+
+// actual code that updates the visitor count
+function visitCount() {
+    if (typeof numVisits() === "object") {
+        window.localStorage.setItem("pageViews", String(1));
+        countVisitsElement.innerHTML = `${message} 1`;
+    } else {
+        countVisits = Number(countVisits) + 1;
+        countVisitsElement.innerHTML = `${message} ${countVisits}`;
+        window.localStorage.setItem("pageViews", countVisits);
+    }
+}
+
+resetbtn.addEventListener("click", () => {
+    countVisits = 1;
+    countVisitsElement.innerHTML = `${message} ${countVisits}`;
+    window.localStorage.setItem("pageViews", countVisits);
+});
+
+function websiteVisits(response) {
+    document.querySelector("#visits").textContent = response.value;
+}
+
+$("#pageViewCountAPI a").on("click", function (event) {
+    event.preventDefault();
+
+    $.ajax(
+        this.href,
+        {
+            success: function (data) {},
+        },
+        error,
+        function () {}
+    );
+});
